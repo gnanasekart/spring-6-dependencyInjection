921a9199d291e05941c058fe5ac65aabd893659f
gnana sekar <gnanasekar69@hotmail.com>
2025-01-20 10:14:37 +0530

    Using Spring default profile Annotation

--- src/main/java/com/tgs/spring6dependencyinjection/services/i18n/EnglishGreetingService.java
+++ src/main/java/com/tgs/spring6dependencyinjection/services/i18n/EnglishGreetingService.java
@@ -4,7 +4,8 @@ import com.tgs.spring6dependencyinjection.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+//@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class EnglishGreetingService implements GreetingService {
     @Override
